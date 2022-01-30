63221461b8b3c95e8543e4620473a1daeb771d29
Julius Platon <j.platon@tu-bs.de>
2022-01-30 19:16:03 +0100

    Refactor code

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var app = express();
 var btoa = require('btoa');
 var session = require('express-session');
 
+
 var base64url = require('base64url');
 var crypto = require('crypto');
 
@@ -24,43 +25,49 @@ app.use(session({
     saveUninitialized: true
 }))
 
-const applicationPort = process.env.port || 3000;
+app.listen(process.env.port || 3000);
 
-app.listen(applicationPort);
+const redirectUri = "http://localhost:3000/redirect";
+const scope = ["read", "write", "openid", "profile", "email", "phone"];
 
-console.log("Express listenting on " + applicationPort)
+const clientId = 1;
+const clientSecret = "sec123"
 
-var asUri = "http://localhost:4567"
+const responseType = ["code", "id_token"]
 
-var tokenEndpoint = "/token"
+var keys;
+var wellKnown;
 
-var redirectUri = "http://localhost:3000/redirect";
+//fetching jwks
+const wellKnownEndpoint = "http://localhost:4567/.well-known/openid-configuration"
 
-var scope = ["read", "write"];
+const fetchAsData = async () => {
+    var wellKnownResponse = await fetch(wellKnownEndpoint)
+    wellKnown = await wellKnownResponse.json()
 
-var clientId = 1;
+    keysResponse = await fetch(wellKnown.jwks_uri)
+    keys = await keysResponse.json()
+}
 
-var clientSecret = "sec123"
+fetchAsData();
 
 app.get("/", (request, response) => {
 
     var state = randomstring.generate(20)
-
     var code_verifier = randomstring.generate(20)
 
     request.session.state = state
-
     request.session.code_verifier = code_verifier
 
     var code_challenge = base64url.fromBase64(crypto.createHash('sha256').update(code_verifier).digest('base64'))
 
     response.render("index.html", {
-        as_uri: `${asUri}/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join("%20")}&state=${state}`
-            + `&code_challenge=${code_challenge}&code_challenge_method=S256&response_type=code`
+        as_uri: `${wellKnown.authorization_endpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scope.join("%20")}&state=${state}`
+            + `&code_challenge=${code_challenge}&code_challenge_method=S256&response_type=${responseType.join("%20")}&nonce=12345`
     })
 })
 
-app.get("/redirect", (request, response) => {
+app.get("/redirect", async (request, response) => {
 
     var state = request.query.state
 
@@ -73,30 +80,31 @@ app.get("/redirect", (request, response) => {
 
     const clientCredentials = clientId + ":" + clientSecret;
 
-    fetch(asUri + tokenEndpoint, {
+    var res = await fetch(wellKnown.token_endpoint, {
         method: 'POST', body: JSON.stringify({ code: code, code_verifier: request.session.code_verifier, grant_type: "authorization_code" }), headers: {
             'Content-Type': 'application/json',
             'Authorization': "Basic " + btoa(clientCredentials),
         }
-    }).then(res => res.json()).then(res => {
-        request.session.token = res.token
-        response.render("redirect.html", {
-            expiry: res.expiry
-        })
+    })
+    var resBody = await res.json()
+    request.session.access_token = resBody.access_token
+    //TODO verifiying id_token signature
+    response.render("redirect.html", {
+        expiry: res.expiry,
+        id_token: Buffer.from(resBody.id_token, 'base64')
+
     })
 })
 
 app.get("/resource", async (request, response) => {
 
-    var res = await fetch("http://localhost:8080/resource", {
+    var res = await fetch(wellKnown.userinfo_endpoint, {
         method: 'GET', headers: {
             'Content-Type': 'application/json',
             'Authorization': "Bearer " + request.session.token,
         }
     })
 
-
-    console.log(res.status)
     if (res.status != 200) {
         response.status(401).send();
         return;
@@ -110,14 +118,13 @@ app.get("/revoke", async (request, response) => {
 
     const clientCredentials = clientId + ":" + clientSecret;
 
-    var res = await fetch("http://localhost:4567/revoke", {
+    var res = await fetch(wellKnown.revocation_endpoint, {
         method: 'POST', body: JSON.stringify({ token: request.session.token }), headers: {
             'Content-Type': 'application/json',
             'Authorization': "Basic " + btoa(clientCredentials)
         }
     })
 
-    console.log(res.status)
     if (res.status != 200) {
         response.status(401).send();
         return;
@@ -126,4 +133,3 @@ app.get("/revoke", async (request, response) => {
     response.status(200).send()
     return;
 })
-
